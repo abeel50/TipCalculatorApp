746d2cc8c0daa5dba708d057a50bad7a27eb018a
abeel50 <abeelmuhammad50@gmail.com>
2023-05-12 11:08:58 +0500

    Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TipCalculatorApp';
+  isFormSubmit: boolean = false;
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,) { }
+
+  ngOnInit() {
+    this.create();
+  }
+
+  create() {
+    this.form = this.formBuilder.group({
+      bill: [null, [Validators.required]],
+      totalPrice: [null, [Validators.required]],
+      products: []
+    });
+  }
+
+  get f() { return this.form.controls; }
 }
